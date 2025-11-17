0e9d5aa3a978fc4220ed1040995a1ccf509a8164
XuXinhNo1 <khaihungpro2005@gmail.com>
2025-11-18 00:45:40 +0700

    Add sys_trace definition

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,12 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_trace(void)
+{
+  int trace_mask;
+  argint(0, &trace_mask);
+  myproc()->mask = trace_mask;
+  return 0;
+}
